c64bbdbad8b2b3cf2000da4671e18c93fb53654f
unknown <abdosker11@yahoo.com>
2022-09-23 11:56:24 +0200

    aboutbaqiradd

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import MainHome from './components/MainHome';
 import MainHomeExpand from './components/MainHomeExpand';
+import AboutBaqir from './components/AboutBaqir';
 import 'bootstrap/dist/css/bootstrap.css';
 import Contact from "./components/Contact"
 import { BrowserRouter, Route, Router, Routes, Link, NavLink } from 'react-router-dom'
@@ -15,6 +16,7 @@ const App = () => {
           <Route path='/' element={<MainHome />} />
           <Route path='/Contact' element={<Contact />} />
           <Route path='/MainHomeExpand' element={<MainHomeExpand />} />
+          <Route path='/AboutBaqir' element={<AboutBaqir />} />
         </Routes>
       </BrowserRouter>
     </div>
